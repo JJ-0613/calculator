002808ad7155cbb86d7fa6988bc8eb2e80dae8be
Wons013 <josephjunelreyes06@gmail.com>
2025-10-17 15:27:09 +0800

    Create arithmetic operations

---
+++ arithmetic.py
@@ -0,0 +1,13 @@
+def get_sum(x,y):
+    return x + y
+
+def get_difference(x,y):
+    return x - y
+
+def get_product(x,y):
+    return x*y
+
+def get_quotient(x,y):
+    if y == 0:
+        raise ValueError("Cannot divide by 0")
+    return x/y
